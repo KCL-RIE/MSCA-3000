dcf91d3dd3c66b4312a8cb179d5b7f9152f2b0ed
Minseok Ryu <minseok5432@gmail.com>
2023-12-31 03:32:51 +0000

    Add test csv file #4 for varied times

--- _archived/create_csv.py
+++ _archived/create_csv.py
@@ -20,21 +20,28 @@ import csv
 #     ('2023-01-03','19:30','20:30'),
 # ]
 
+# data_og_3 = [
+#     ('2024-05-01','16:00','18:00'),
+#     ('2023-04-15','11:00','14:30'),
+#     ('2025-01-01','12:00','12:30'),
+#     ('2022-11-30','11:30','13:30'),
+# ]
+
 data = [
-    ('2024-05-01','16:00','18:00'),
-    ('2023-04-15','11:00','14:30'),
-    ('2025-01-01','12:00','12:30'),
-    ('2022-11-30','11:30','13:30'),
+    ('2022-11-30','15:00','17:37'),
+    ('2023-01-01','13:25','14:32'),
+    ('2023-01-01','16:00','16:45'),
+    ('2023-01-01','18:00','18:30'),
 ]
 
 # Write CSV file
-with open("datetimes3.csv", "wt") as fp:
+with open("datetimes4.csv", "wt") as fp:
     writer = csv.writer(fp, delimiter=",")
     # writer.writerow(["your", "header", "foo"])  # write header
     writer.writerows(data)
 
 # Read CSV file
-with open("datetimes3.csv") as fp:
+with open("datetimes4.csv") as fp:
     reader = csv.reader(fp, delimiter=",", quotechar='"')
     # next(reader, None)  # skip the headers
     data_read = [row for row in reader]
